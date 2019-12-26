5f1d8a4bd1cba1bf43d661a863643bdd68c4f27c
AShimanskaya <99nastyusha99@gmail.com>
2019-12-27 01:49:57 +0300

    [CUR-10] Add Sendgrid as mail delivery service

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,19 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+# Setup the mailer config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => ENV['SENDGRID_DOMAIN'],
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
